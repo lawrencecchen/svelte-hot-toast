18e02506e95343d6d1c44c2dddfa68ceeaac6298
Lawrence Chen <lawrencechen2002@gmail.com>
2021-11-22 19:00:01 -0800

    update config

--- svelte.config.js
+++ svelte.config.js
@@ -10,9 +10,6 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		package: {
-			exports: (file) => file === 'index.js' || 'dist/svelte-hot-toast.css'
-		},
 		vite: {
 			resolve: {
 				alias: {
