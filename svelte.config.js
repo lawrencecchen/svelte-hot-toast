c7063249501df17eb5a11b4d43237a2aaeb4ae6b
Lawrence Chen <lawrencechen2002@gmail.com>
2021-11-22 19:05:16 -0800

    0.0.11

--- svelte.config.js
+++ svelte.config.js
@@ -10,6 +10,9 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
+		package: {
+			exports: (file) => file === 'index.ts'
+		},
 		vite: {
 			resolve: {
 				alias: {
